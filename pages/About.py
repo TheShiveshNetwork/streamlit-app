08c30d426085c8ba9b6c807dcf88c5663928dea4
unknown <shiveshtiwari0@gmail.com>
2023-11-13 23:10:37 +0530

    project setup

---
+++ pages/About.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('About')
+
+st.write('''
+            This is a fun Project made with
+            - Streamlit
+        ''')
