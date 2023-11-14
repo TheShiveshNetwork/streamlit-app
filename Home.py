7015723d9509fc913ff1b24fd787859830f3d688
unknown <shiveshtiwari0@gmail.com>
2023-11-14 21:23:34 +0530

    changes

--- Home.py
+++ Home.py
@@ -1,23 +1,10 @@
 import streamlit as st
 
-st.title("Chat with me")
+st.title("Home")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+st.markdown("""
+            This app is a collection of projects made with Streamlit.
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input("What is up?"):
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-    response = f"Echo {prompt}"
-
-    with st.chat_message("assistant"):
-        st.markdown(response)
-
-    st.session_state.messages.append({"role": "assistant", "content": response})
+            ## Projects
+            1. [Sentiment Analysis](/Sentiment_Analysis)
+        """)
