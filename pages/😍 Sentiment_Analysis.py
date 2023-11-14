7b418b8f98dceaea1df7e8aaf068fd32a691451b
unknown <shiveshtiwari0@gmail.com>
2023-11-14 21:01:49 +0530

    sentiment alalysis complete

--- pages/😍 Sentiment_Analysis.py
+++ pages/😍 Sentiment_Analysis.py
@@ -11,5 +11,18 @@ text = st.text_area("Enter the text", placeholder="This is a sample text")
 
 if text:
     blob = textblob.TextBlob(text)
-    st.write('Polarity: ', round(blob.sentiment.polarity, 2))
-    st.write('Subjectivity: ', round(blob.sentiment.subjectivity, 2))
+    text_polarity = round(blob.sentiment.polarity, 2)
+    # st.write('Polarity: ', text_polarity)
+    text_subjectivity = round(blob.sentiment.subjectivity, 2)
+    # st.write('Subjectivity: ', text_subjectivity)
+
+    if text_polarity > 0:
+        st.markdown("Sentiment: Positive :smiley: ")
+    elif text_polarity < 0:
+        st.markdown("Sentiment: Negative :angry: ")
+    else:
+        st.markdown("Sentiment: Neutral 😐 ")
+        
+    sentiment_dict = {"polarity": text_polarity, 'subjectivity': text_subjectivity}
+    sentiment_df = pd.DataFrame(sentiment_dict.items(), columns=['metric', 'value'])
+    st.dataframe(sentiment_df)
